3c4cd2c3495825ae0e46e6910ed98adf65556b74
wjun7890 <93869093+wjun7890@users.noreply.github.com>
2023-03-31 00:44:54 +0800

    Update app.R

--- app.R
+++ app.R
@@ -1,4 +1,12 @@
-
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+install.package("shiny")
 library(shiny)
 # Define UI for application that draws a histogram
 ui <- fluidPage(
